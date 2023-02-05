baaa04ec67386f49bdda153faa4c56636caa56ec
turnPenny <bohdan.miroshnik23@gmail.com>
2023-02-05 15:40:43 +0100

    saveTokenHandler function

--- weather.js
+++ weather.js
@@ -2,6 +2,15 @@ import { getArgs } from './helpers/args.js';
 import { printHelp, printError, printSuccess } from './services/log_service.js';
 import { saveToken } from './services/storage.service.js';
 
+const saveTokenHandler = async (token) => {
+  try {
+    await saveToken('token', token);
+    printSuccess('token is saved');
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
 const initCli = () => {
   const args = getArgs(process.argv);
   if (args.h) {
@@ -11,7 +20,7 @@ const initCli = () => {
     printSuccess();
   }
   if (args.t) {
-    saveToken('token', args.t);
+    return saveTokenHandler(args.t);
   }
 };
 
