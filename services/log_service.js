27ab188c54b5a20228522a04b9ed31f85c0a61b6
turnPenny <bohdan.miroshnik23@gmail.com>
2023-02-12 19:04:51 +0100

    main functional is conmpleted

--- services/log_service.js
+++ services/log_service.js
@@ -22,4 +22,16 @@ const printHelp = () => {
   );
 };
 
-export { printError, printSuccess, printHelp };
+const printWeather = (res) => {
+  console.log(
+    dedent`${chalk.bgYellow(' WEATHER ')} weather in city: ${res.name}
+    ${chalk.bgCyan(` ${res.weather[0].description}`)}
+    ${chalk.bgCyanBright('Temperature:')} ${res.main.temp} (feels like ${
+      res.main.feels_like
+    })
+    ${chalk.bgGreenBright('Wind speed:')}  ${res.wind.speed}
+    `
+  );
+};
+
+export { printWeather, printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,22 +1,41 @@
 import { getArgs } from './helpers/args.js';
 import { getWeather } from './services/api_service.js';
-import { printHelp, printError, printSuccess } from './services/log_service.js';
+import {
+  printHelp,
+  printError,
+  printSuccess,
+  printWeather,
+} from './services/log_service.js';
 import {
   getToken,
+  saveCity,
   saveToken,
   TOKEN_DICTIONARY,
 } from './services/storage.service.js';
 
 const saveTokenHandler = async (token) => {
   if (!token.length) {
-    printError('The token has not been transferred');
+    printError('The token has not been passed');
     return;
   }
   try {
     await saveToken(TOKEN_DICTIONARY.token, token);
     printSuccess('token is saved');
-  } catch (error) {
-    printError(error.message);
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const saveCityHandler = async (city) => {
+  if (!city.length) {
+    printError('The city has not been passed');
+    return;
+  }
+  try {
+    await saveCity(TOKEN_DICTIONARY.city, city);
+    printSuccess('city is saved');
+  } catch (e) {
+    printError(e.message);
   }
 };
 
@@ -24,7 +43,7 @@ const getForecast = async () => {
   try {
     const city = await getToken(TOKEN_DICTIONARY.city);
     const weather = await getWeather(city);
-    console.log(weather);
+    printWeather(weather);
   } catch (e) {
     if (e?.process?.statuc == 404) {
       printError('wrong city name');
@@ -42,7 +61,7 @@ const initCli = () => {
     printHelp();
   }
   if (args.s) {
-    printSuccess();
+    return saveCityHandler(args.s);
   }
   if (args.t) {
     return saveTokenHandler(args.t);
